d1450ef3084570c87242f534926381396e4cf369
NobodyWHO <Somebody@else.globale>
2023-07-01 10:13:19 -0400

    first commit

---
+++ hello_linux_d/hellolin.c
@@ -0,0 +1,17 @@
+/*
+*	this is the first project ever
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    char value[20];
+    puts("this is the first code on linux \n");
+    printf("hello linux\n:");
+    scanf("%19s",value);
+    printf("welcome to home %s\n",value);
+
+    return 0;
+}
